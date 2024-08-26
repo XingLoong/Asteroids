d9632c9afb7513bd7769945f1ea8af985d6c3c85
XingLoong <xing.loong@gmail.com>
2024-08-26 20:03:54 +1000

    3.6: add chaos

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshape import *
 from constants import *
 
@@ -11,3 +12,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += dt * self.velocity
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        new_velocity1 = self.velocity.rotate(-random_angle)
+        new_velocity2 = self.velocity.rotate(random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = new_velocity1 * 1.2
+        asteroid2.velocity = new_velocity2 * 1.2
+        
